7d7625af0ada1b9c4769269948073df4eb61f6f5
Matt Petermann <MPetermann2@my.wctc.edu>
2019-08-26 10:41:57 -0500

    Modas week 1

---
+++ Modas/Modas/Modas/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Modas.Models;
+namespace Modas.Controllers
+{
+	public class HomeController : Controller
+	{
+		private IEventRepository repository;
+		public HomeController(IEventRepository repo)
+		{
+			repository = repo;
+		}
+
+		public ViewResult Index() => View(repository.Events);
+	}
+}
