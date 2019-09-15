d806cfc0c2084726bfd455a2e1cef0707b405253
MATTPC\Matt <matthewp214@gmail.com>
2019-09-15 17:31:00 -0500

    Add Modas with EF core project

---
+++ Modas - EF/Modas/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Modas.Models;
+
+namespace Modas.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ViewResult Index() => View(FakeEventRepository.Events);
+	}
+}
---
+++ Modas - EF/Modas/Models/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Modas.Models
+{
+	public class Event
+	{
+		public Guid EventId { get; set; }
+		public DateTime TimeStamp { get; set; }
+		public bool Flagged { get; set; }
+        private int locationId { get; set; }
+		public int LocationId {
+            get => locationId;
+            set {
+                locationId = value;
+                Location = FakeEventRepository.Locations.Single(l => l.LocationId == value);
+            }
+        }
+		public Location Location { get; set; }
+	}
+}
