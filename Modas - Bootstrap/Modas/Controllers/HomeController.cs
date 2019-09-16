0dcfb5fe8ed99422b9f4a6f31841044d9d5d432c
MATTPC\Matt <matthewp214@gmail.com>
2019-09-15 23:36:37 -0500

    Working buttons

--- Modas - Bootstrap/Modas/Controllers/HomeController.cs
+++ Modas - Bootstrap/Modas/Controllers/HomeController.cs
@@ -1,10 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Modas.Models;
 
 namespace Modas.Controllers
 {
 	public class HomeController : Controller
 	{
-		public ViewResult Index() => View(FakeEventRepository.Events);
+		public ViewResult Index(int pageNumber = 1, int resultCount = 10)
+        {
+            return View( new IndexViewModel
+            {
+                Events = FakeEventRepository.Events.Skip((pageNumber - 1) * resultCount).Take(resultCount),
+                MaxPages = (int)Math.Ceiling(FakeEventRepository.Events.Count / (resultCount + 0.0)),
+                CurrentPage = pageNumber,
+                ResultCount = resultCount
+            });
+        } 
 	}
 }
---
+++ Modas - Bootstrap/Modas/Models/IndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Modas.Models
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<Event> Events { get; set; }
+        public int MaxPages { get; set; }
+        public int CurrentPage { get; set; }
+        public int ResultCount { get; set; }
+    }
+}
