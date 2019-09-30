d4e3701f5befb604b4705695c5bbd824352eff2d
Matt Petermann <MPetermann2@my.wctc.edu>
2019-09-30 08:41:25 -0500

    Duplicate bootstrap project for ajax requests

---
+++ Modas - Ajax/Modas/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Modas.Models;
+
+namespace Modas.Controllers
+{
+	public class HomeController : Controller
+	{
+        private EFEventRepository repository;
+        public HomeController(EFEventRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index(int pageNumber = 1, int resultCount = 10) => 
+            View(new IndexViewModel
+            {
+                Events = repository.Events.Skip((pageNumber - 1) * resultCount).Take(resultCount),
+                Locations = repository.Locations,
+                EventCount = repository.Events.Count(),
+                MaxPages = (int)Math.Ceiling(repository.Events.Count() / (resultCount + 0.0)),
+                CurrentPage = pageNumber,
+                ResultCount = resultCount
+            });
+	}
+}
---
+++ Modas - Ajax/Modas/Models/EFEventRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace Modas.Models
+{
+    public class EFEventRepository
+    {
+        private ApplicationDbContext context;
+
+        public EFEventRepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IQueryable<Event> Events => context.Events;
+        public IQueryable<Location> Locations => context.Locations;
+    }
+}
